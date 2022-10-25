59f35737ba97af05fff6a45b9532404d6d2dab56
its-imba <its.sam.imba@gmail.com>
2022-10-25 18:27:40 +0100

    revert changes

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from venv import create
 from website import create_app
-from . import db
-
-
 
 app = create_app()
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
