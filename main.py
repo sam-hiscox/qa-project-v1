a8759569da8b90ea2093828e342b4d3aa1a9de88
its-imba <its.sam.imba@gmail.com>
2022-10-26 11:52:24 +0100

    db changes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from venv import create
 from website import create_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = create_app()
+
+db = SQLAlchemy(app)
+
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dkd93kshaghj429'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/samhiscox/db/database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
