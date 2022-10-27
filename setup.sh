c7108b024aaa34ac110ec18455b7f3eb5272e599
Sam H <97249915+its-imba@users.noreply.github.com>
2022-10-27 12:20:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "setup stage"
-sudo apt-get update
-sudo apt-get install -y curl jq
+apt-get update
+apt-get install -y curl jq
 
 curl https://get.docker.com | sudo bash
-sudo usermod -aG docker jenkins
+usermod -aG docker jenkins
