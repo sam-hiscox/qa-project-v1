a8e834c4fbefb8e1c0dc27b950e54ee4015376aa
Sam H <97249915+its-imba@users.noreply.github.com>
2022-10-27 13:00:42 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ echo "building app..."
 
 cd /home/jenkins/.jenkins/workspace/pipeline-1/website
 
+sudo chown "$USER":"$USER" /var/run/docker.sock -R
+sudo chmod g+rwx "/var/run/docker.sock" -R
+
 docker build -t myapp .
