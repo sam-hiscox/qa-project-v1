84ddb26d61d312e443eaa01c1eb4dbd5115afc57
Sam H <97249915+its-imba@users.noreply.github.com>
2022-11-20 12:52:50 +0000

    remove -s

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "setting up docker..."
-sudo -s apt-get update
-sudo -s apt-get install -y curl jq
+sudo apt-get update
+sudo apt-get install -y curl jq
 
-sudo -s curl https://get.docker.com | sudo bash
-sudo -s usermod -aG docker jenkins
+sudo curl https://get.docker.com | sudo bash
+sudo usermod -aG docker jenkins
